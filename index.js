cbbad02b4ec1f6578c651aca2a6b2effd5e94d37
Nabi shafin <shafin21215@gmail.com>
2024-09-19 17:36:07 +0600

    setup or complete

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('btn').addEventListener('click', function (event) {
+    event.preventDefault()
+
+    const number = document.getElementById('number').value;
+    console.log(number)
+
+})
