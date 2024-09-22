d03c425f0ac10715ada89d4eb475710866b7c532
nabishafin <shafin21215@gmail.com>
2024-09-22 19:43:18 +0600

    ok Done

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 document.getElementById('btn').addEventListener('click', function (event) {
     event.preventDefault()
+    const numberInput = document.getElementById('number').value;
+    const pinInput = document.getElementById('password').value;
+    // const number = parseInt(numberInput);
+    // const pin = parseInt(pinInput);
+    if (numberInput.length < 11 && pinInput.length == 4) {
+        console.log('ok')
+    }
+    else {
+        console.log('plz try again')
+    }
 
-    const number = document.getElementById('number').value;
-    console.log(number)
+
+
+
+
 
 })
